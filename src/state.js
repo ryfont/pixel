aa6d8398327d2fb4755fe246f4ef840f4e37fa92
Robert Lord <robert@lord.io>
2018-03-17 15:07:32 -0700

    Add current player management

--- src/state.js
+++ src/state.js
@@ -7,7 +7,9 @@ export class Game {
   initialize (gameId) {
     this.onupdate = []
     this.state = null
-    firebase.database().ref(`games/${gameId}`).on('value', snap => {
+    this.currentPlayer = null // role of this player, either 'red' 'blue' 'judge' or null
+    this.dbref = firebase.database().ref(`games/${gameId}`)
+    this.dbref.on('value', snap => {
       this.state = snap.val()
       this.onupdate.forEach(f => { f() })
     })
@@ -16,6 +18,25 @@ export class Game {
   onupdate (f) {
     this.onupdate.push(f)
     f()
+  }
+
+  players () {
+    this.state.players
+  }
+
+  currentPlayer () {
+    return this.currentPlayer
+  }
+
+  setCurrentPlayer (player) {
+    // TODO update database to reject new edits of player
+    return this.dbref
+      .child('players').child(player)
+      .set(1)
+      .then(() => {
+        // successfully set player
+        this.currentPlayer = player
+      })
   }
 
   // generates new game code and creates new game for it, and returns a promise containing the new game object
@@ -36,9 +57,9 @@ export class Game {
         .then(snap => {
           if (snap.val() === null) {
             gameState.set({
-              drawings: {red: null, blue: null},
+              drawings: {red: {}, blue: {}},
               image: "",
-              pixels: {},
+              pixels: {red: {}, blue: {}},
               players: {blue: 0, judge: 0, red: 0}
             })
             resolve(newId)
